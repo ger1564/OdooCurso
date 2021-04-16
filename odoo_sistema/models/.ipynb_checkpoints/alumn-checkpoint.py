0f8938b27d3b86ab477d7c96e598bb79a1528c3b
ger1564 <gexo1564@gmail.com>
2021-04-16 18:57:26 +0000

    [Campos alumno]

--- odoo_sistema/models/.ipynb_checkpoints/alumn-checkpoint.py
+++ odoo_sistema/models/.ipynb_checkpoints/alumn-checkpoint.py
@@ -4,5 +4,20 @@ from odoo import models, fields, api
 
 class Alumn(models.Model):
     
-    _name = 'sistema.course'
+    _name = 'sistema.alumn'
     _description = 'Información del Alumno'
+    
+    numero_cuenta_alum = fields.Integer(string='Número de cuenta', required=True)
+    nombre_per = fields.Char(string='Nombre', required=True)                   
+    materno_per = fields.Char(string='Apellido materno', required=True)    			
+    paterno_per = fields.Char(string='Apellido paterno', required=True)      				
+    email_per = fields.Char(string='Correo electrónico', required=True)       				 	
+    login_per = fields.Char(string='Usuario', required=True)         					
+    password_per = fields.Char(string='Contraseña', required=True)       			   		
+    fecha_nacimiento_per = fields.Date(string='Fecha de nacimiento', required=True)
+    active = fields.Boolean(string='Activo',default=True)
+    career = fields.Selection(string='Carrera',selection=[('computer','Computación'),
+                                                            ('telecom', 'Telecomunicaciones'),
+                                                            ('mechanic','Mecánica'),
+                                                            ('electronic','Electronica')],
+                                                            copy=False)
