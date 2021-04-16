ecf90f00f654e39dbf9716fc2e70ed106c78351b
ger1564 <gexo1564@gmail.com>
2021-04-16 05:16:10 +0000

    [módulo Sistema]

---
+++ odoo_sistema/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo SISTEMA',
+    'summary': """Ejercicio SISTEMA""",
+    'description': """
+                    Ejercicio SISTEMA:
+                    -Alumno
+                    -Profesor
+                    -Asignatura
+    """,
+    'author':'GLM',
+    'website':'https://www.odoo.com',
+    'category': 'Curso Odoo',
+    'version':'0.1',
+    'depends': ['base'],
+    'data': [
+    
+    ],
+    'demo': [
+	
+    ],
+    
+}
